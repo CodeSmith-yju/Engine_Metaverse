e25707308e05ce7a0481e99855e08a7c5e693eab
kalrabear <kalrabears@gmail.com>
2024-04-16 23:19:38 +0900

    Order Processing, Role
    주문 처리 과정 버그 수정 및 여러번 상호작용 되는 문제 해결
    
    권한이 없으면 상호작용이 안되도록 하는 코드 추가

--- Metaverse_Team5/Assets/Scripts/Object/Interactive.cs
+++ Metaverse_Team5/Assets/Scripts/Object/Interactive.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class Interactive : MonoBehaviour
 {
     bool isEnter = false;
+    bool isCheck = false;
     string ingredient = null;
     bool isHot = false;
+    Player player;
 
     // 지금은 오브젝트 이름으로 관리 더 좋은 방법이 있으면 그걸로 바꿀 예정
     private void Start()
@@ -50,6 +52,25 @@ public class Interactive : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (isCheck && isEnter) 
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                InteractWithPlayer(player);
+            }
+        }
+        else if (!isCheck && isEnter)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("점장이거나 직원만 상호작용 가능한 오브젝트입니다.");
+                return;
+            }
+        }
+    }
+
     // 플레이어 태그를 가진 오브젝트와 부딪치면 오브젝트와 충돌 체크 테스트용 ( 나중에는 레이캐스트를 이용해서 사물을 바라보고 있을 때 쓸 할 예정 )
     private void OnCollisionEnter(Collision collision)
     {
@@ -61,57 +82,80 @@ public class Interactive : MonoBehaviour
 
     private void OnCollisionStay(Collision collision)
     {
-        Player player = collision.gameObject.GetComponent<Player>();
+        player = collision.gameObject.GetComponent<Player>();
 
         if (isEnter && collision.collider.CompareTag("Player") && player.GetRole() == Role.Manager || player.GetRole() == Role.Empolyee)
         {
-            
+            isCheck = true;
+        }
+    }
 
-            if (Input.GetKey(KeyCode.Space))
+    private void OnCollisionExit(Collision collision)
+    {
+        isCheck = false;
+        isEnter = false;
+    }
+
+    private void InteractWithPlayer(Player player)
+    {
+        if (name == "Done")
+        {
+            if (player.cup && player.done)
             {
-                if (name == "Done" && player.cup && player.done)
-                {
-                    Debug.Log("제작 완료 손님 전달");
-                    player.Done();
-                }
-                else if (name == "Create" && player.cup)
-                {
-                    Debug.Log("제작 시도");
-                    player.Create();
-                }
-                else if (name == "Role")
-                {
-                    if (player.GetRole() == Role.Manager) // 권한이 매니저일 경우에만 사용가능하게
-                    {
-                        Debug.Log("권한 설정");
-                        GameMgr.Instance.player_List[0].gameObject.GetComponent<Player>().SetRole(Role.Empolyee); 
-                        // 이 코드를 플레이어 리스트를 가져와서 UI에서 선택하는 걸로 바꿀 예정
-                    }
-                }
-                else if (name == "Cup")
-                {
-                    if (player.cup == false)
-                    {
-                        Debug.Log("손에 컵 듦");
-                        player.cup = true;
-                    }
-                    else
-                    {
-                        Debug.Log("이미 컵을 들고 있음");
-                    }
-                }
-                else
-                {
-                    if (player.done == false && player.cup == true)
-                    {
-                        player.cur_IngrList.Add(ingredient);
-                        Debug.Log(ingredient + " 재료 추가");
-                    }
-                }
-
-                isEnter = false;
+                Debug.Log("제작 완료 손님 전달");
+                player.Done();
+            }
+            else
+            {
+                Debug.Log("완성된 음료를 가지고 있지 않거나, 만들지 않았습니다.");
+                return;
             }
 
+        }
+        else if (name == "Create" && player.cup)
+        {
+            Debug.Log("제작 시도");
+            player.Create();
+        }
+        else if (name == "Role")
+        {
+            if (player.GetRole() == Role.Manager) // 권한이 매니저일 경우에만 사용가능하게
+            {
+                Debug.Log("권한 설정");
+                GameMgr.Instance.player_List[0].gameObject.GetComponent<Player>().SetRole(Role.Empolyee);
+                // 이 코드를 플레이어 리스트를 가져와서 UI에서 선택하는 걸로 바꿀 예정
+            }
+            else
+            {
+                Debug.Log("권한이 없습니다.");
+                return;
+            }
+        }
+        else if (name == "Cup")
+        {
+            if (player.cup == false)
+            {
+                Debug.Log("손에 컵 듦");
+                player.cup = true;
+            }
+            else
+            {
+                Debug.Log("이미 컵을 들고 있음");
+                return;
+            }
+        }
+        else
+        {
+            if (player.done == false && player.cup == true && name != "Done" && name != "Role")
+            {
+                player.cur_IngrList.Add(ingredient);
+                Debug.Log(ingredient + " 재료 추가");
+            }
+            else
+            {
+                Debug.Log("컵을 들고 있지 않습니다.");
+                return;
+            }
         }
     }
 }
